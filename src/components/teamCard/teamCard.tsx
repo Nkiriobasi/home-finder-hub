b2206d55102ad0f2459caf2943f8ef8c982c5c9f
Nkiriobasi <obasinkiri@gmail.com>
2023-08-23 19:15:08 +0100

    modified team section UI

--- src/components/teamCard/teamCard.tsx
+++ src/components/teamCard/teamCard.tsx
@@ -1,31 +1,37 @@
 import Image from "next/image"
 
 import HeadingText from "../headingText/headingText"
-import DavisCarder from '../../../public/davis-carder.svg'
 import ParagraphText from "../paragraphText/paragraphText"
 
 interface TeamCardPropsType {
-    img?: any
+    imgSrc: any;
+    name: string;
+    position: string;
 }
 
-const TeamCard:React.FC<TeamCardPropsType> = ({ img }) => {
+const TeamCard:React.FC<TeamCardPropsType> = ({ imgSrc, name, position }) => {
   return (
-    <div className="max-w-[350px] block bg-white dark:bg-[#1e1e1e] rounded-xl shadow cursor-pointer">
+    <div className="max-w-full block bg-white dark:bg-[#1e1e1e] rounded-xl shadow">
         <div className="w-full relative flex flex-col items-center p-6">
             <div className="max-w-[200px] flex items-center justify-center">
-                <Image src={DavisCarder} alt='team member' className="w-full rounded-full" priority />
+                <Image 
+                    src={imgSrc} 
+                    alt='team member' 
+                    className="w-full rounded-full" 
+                    priority 
+                />
             </div>
 
             <div className="mt-[10px] flex flex-col text-center">
                 <span className="inline-block">
-                    <HeadingText styles="lg:text-[20px] sm:text-[16px] text-sm mb-1 text-[#141B2D] dark:text-white">
-                        Davis Carder
+                    <HeadingText styles="lg:text-[20px] sm:text-[18px] text-base mb-1 text-[#141B2D] dark:text-white">
+                        {name}
                     </HeadingText>
                 </span>
 
                 <span className="inline-block">
-                    <ParagraphText styles="md:text-base text-sm text-[#141B2D] dark:text-white">
-                        Super duper position
+                    <ParagraphText styles="text-sm text-[#141B2D] dark:text-white">
+                        {position}
                     </ParagraphText>
                 </span>
             </div>
--- src/layouts/homepage/ourTeam/ourTeam.tsx
+++ src/layouts/homepage/ourTeam/ourTeam.tsx
@@ -1,12 +1,44 @@
+import HeadingText from "@/components/headingText/headingText"
+import ParagraphText from "@/components/paragraphText/paragraphText"
 import TeamCard from "@/components/teamCard/teamCard"
+import DavisCarder from '../../../../public/davis-carder.svg'
+import MarenPress from '../../../../public/maren-press.svg'
+import RandyRosser from '../../../../public/randy-rosser.svg'
+import HaylieDonin from '../../../../public/haylie-donin.svg'
+import MiracleBator from '../../../../public/miracle-bator.svg'
 
 
 const OurTeam = () => {
   return (
     <section className='w-full max-w-full flex'>
-        <div className="max-w-[1287px] mx-auto w-full md:pb-[80px] pb-[50px] pt-[20px] sm:px-5 px-[14px] flex flex-col">
-            <TeamCard />
+      <div 
+        className="max-w-[1287px] mx-auto w-full md:pb-[80px] pb-[50px] 
+        pt-[25px] sm:px-5 px-[14px] flex flex-col"
+      >
+        <div className="grid md:grid-cols-3 xs:grid-cols-2 gap-5">
+          <div className="max-w-full flex flex-col items-start justify-center">
+            <div className="w-full relative flex flex-col items-center justify-center">
+              <span className="inline-block w-full">
+                <HeadingText styles="mb-5 lg:text-[44px] md:text-[34px] text-[24px]">
+                  Our Team
+                </HeadingText>
+              </span>
+
+              <span className="w-full inline-block">
+                <ParagraphText styles="lg:text-[18px] md:text-[16px] text-sm">
+                  We provide a complete service for the sale, purchase or rental of real estate. 
+                  We have been operating in Madrid and Barcelona more than 15 years.
+                </ParagraphText>
+              </span>
+            </div>
+          </div>
+          <TeamCard imgSrc={DavisCarder} name="Davis Carder" position="Super duper position" />
+          <TeamCard imgSrc={MarenPress} name="Maren Press" position="Super duper position" />
+          <TeamCard imgSrc={RandyRosser} name="Randy Rosser" position="Super duper position" />
+          <TeamCard imgSrc={HaylieDonin} name="Haylie Donin" position="Super duper position" />
+          <TeamCard imgSrc={MiracleBator} name="Miracle Bator" position="Super duper position" />
         </div>
+      </div>
     </section>
   )
 }
